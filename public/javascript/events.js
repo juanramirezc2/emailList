5f7c6babd62d7149f134506ec3f217513e939b97
juan gabriel ramirez <jgramirez@xumak.com>
2015-08-10 11:55:56 -0500

    events and date diff added

--- public/javascript/events.js
+++ public/javascript/events.js
@@ -5,7 +5,8 @@ function EmailEvents(mainElem){
 EmailEvents.prototype={
   constructor:EmailEvents,
   init:initFunc,
-  checkRead:checkReadFunc
+  checkRead:checkReadFunc,
+  updateHour:updateHour
 }
 function initFunc(el){
   var Self=this;
@@ -13,12 +14,15 @@ function initFunc(el){
   var showmore=document.getElementById("showRead");
   for(var i=0;i<links.length;i++){
     links[i].addEventListener("click",function(e){
-      document.getElementById("from").innerHTML="from: "+this.dataset.fromname;
       document.getElementById("subject").innerHTML="subject";
       document.getElementById("content").innerHTML=this.dataset.content;
-      document.getElementById("to").innerHTML=this.dataset.content;
+      document.getElementById("to").innerHTML="to : not provided";
+      var from=this.querySelector(".emailList__from").innerHTML;
+      document.getElementById("from").innerHTML="from: "+from;
       var subject=this.querySelector(".emailList__subject").innerHTML;
       document.getElementById("subject").innerHTML="subject: "+subject;
+      var received=this.querySelector(".emailList__received").innerHTML;
+      document.getElementById("received").innerHTML="received: "+received;
       e.preventDefault();
     }
     );
@@ -32,7 +36,17 @@ function initFunc(el){
    }
    
   });
+  this.updateHour(links);
 
+}
+function updateHour(links){
+  for(var i=0;i<links.length;i++){
+    var time=links[i].querySelector(".emailList__received");
+    var diff=Date.diff(Date.now(),Number(time.innerHTML));
+    var diffFormat=diff.days +"days,"+diff.hours+" hours,"+diff.minutes+" minutes Ago";
+    time.innerHTML=diffFormat;
+    console.log(diffFormat);
+  }
 }
 function checkReadFunc(links,state){
   for(var i=0;i<links.length;i++){
